730f1b87e3aa66524beb014c5a3f043fad5ac03f
Carsten Burgard <cburgard@cern.ch>
2018-11-16 17:01:28 +0100

    added batch submission of scan jobs (experimental)

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,4 +1,9 @@
 #!/bin/env python
+
+_noDelete = []
+def noDelete(something):
+    _noDelete.append(something)
+
 try:
     isinstance("", basestring)
     def isstr(s):
@@ -47,7 +52,6 @@ def mod(a,b):
 def linspace(vmin,vmax,npoints):
     step = (vmax - vmin)/(npoints)
     vals = [ float(vmin + i*step) for i in range(0,npoints+1) ]
-    print(vals)
     return vals
 
 def loadRooFitUtils():
@@ -184,6 +188,21 @@ def buildMinimizer(args,model):
     minimizer = ROOT.RooFitUtils.ExtendedMinimizer("minimizer", model,arglist)
     return minimizer
 
+
+def generateCoordsDict(scan):
+    parname = args.scan[0]
+    parrange = linspace(float(args.scan[2]),float(args.scan[3]),int(args.scan[1]))
+    return [ {parname:val} for val in parrange ]
+
+def generateCoordsVec(scan):
+    parname = args.scan[0]
+    parrange = linspace(float(args.scan[2]),float(args.scan[3]),int(args.scan[1]))
+    parnames = vec([parname],"string")
+    return vec([ vec([val],"double") for val in parrange],"vector<double>")
+
+def parsePoint(line):
+    return { n.strip():float(v) for (n,v) in [x.split("=") for x in line.split(",") ]}
+
 def fit(args,model,minimizer):
     from time import time
     import ROOT
@@ -241,16 +260,18 @@ def fit(args,model,minimizer):
     parnames = None
     coords = None
     if args.scan:
-        parname = args.scan[0]
-        parrange = linspace(float(args.scan[2]),float(args.scan[3]),int(args.scan[1]))
-        parnames = vec([parname],"string")
-        coords = vec([ vec([val],"double") for val in parrange],"vector<double>")
+        coords = generateCoordsVec(args.scan)
 
     if args.points != None:
         with open(args.points) as infile:
-            points = [ { n.strip():float(v) for (n,v) in [x.split("=") for x in line.split(",") ]} for line in infile if len(line)>0]
-        parnames = vec(sorted(union([p.keys() for p in points])),"string")
-        coords = vec( [ vec( [ point[p] for p in parnames ] , "double") for point in points ], "vector<double>")
+            points = [ parsePoint(line) for line in infile if len(line)>0 ]
+            parnames = vec(sorted(union([p.keys() for p in points])),"string")
+            coords = vec( [ vec( [ point[p] for p in parnames ] , "double") for point in points ], "vector<double>")
+
+    if args.point != None:
+        point = parsePoint(args.point)
+        parnames = vec(sorted(point.keys()),"string")
+        coords = vec( [ vec( [ point[p] for p in parnames ] , "double") ], "vector<double>")
 
     if parnames and coords and not args.dummy:
         minimizer.scan(parnames,coords)
@@ -284,56 +305,104 @@ def fit(args,model,minimizer):
         ws = model.GetWorkspace()        
         ws.writeToFile(args.outWsName)
 
+def reconstructCall(args,arglist,blacklist):
+    options = {}
+    from argparse import _StoreTrueAction,_StoreFalseAction,_StoreAction
+    for arg in args.__dict__:
+        actions = []
+        for action in arglist:
+            if arg in blacklist: continue
+            if arg == action.dest:
+                argval = args.__dict__[arg]
+                argopt = action.option_strings[0]
+                if argval == True and isinstance(action,_StoreTrueAction):
+                    options[argopt]=None
+                if argval == False and isinstance(action,_StoreFalseAction):
+                    options[argopt]=None
+                if isinstance(action,_StoreAction) and not argval == action.default:
+                    options[argopt]=argval
+    return options
+
+def stringify(s):
+    if s == None: return ""
+    if isinstance(s,list): return " ".join(['"'+v+'"' for v in s])
+    return str(s)
+
+def createScanJobs(args,arglist):
+    options = reconstructCall(args,arglist,["scan","findSigma","writeSubmit"])
+    import sys
+    name = sys.argv[0]
+    coords = generateCoordsDict(args.scan)
+    idx = 0
+    with open(args.writeSubmit,"wt") as jobs:
+        for coord in coords:
+            point = ",".join([k+"="+str(v) for k,v in coord.items()])
+            options["--singlepoint"]=point
+            options["--output"]=args.outFileName+".part"+str(idx)
+            cmd = " ".join([k+" "+stringify(v) for k,v in options.items()])
+            jobs.write(name+" "+cmd+"\n")
+            idx = idx+1
+    print("wrote "+args.writeSubmit)
+
+
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser("run a fit")
-    parser.add_argument( "--input"         , type=str,     dest="inFileName"                 , help="File to run over.", required=True, metavar="path/to/workspace.root")
-    parser.add_argument( "--output"        , type=str,     dest="outFileName"                , help="Output file.", required=False, metavar="out.txt")
-    parser.add_argument( "--poi"           , type=str,     dest="pois"                       , help="POIs to measure.", metavar="POI", nargs="+", default=[])
-    parser.add_argument( "--scan"          , nargs=4,      dest="scan"                       , help="POI range to scan the Nll.", metavar=("POI","n","min","max"), default=None)
-    parser.add_argument( "--points"        , type=str,     dest="points"                     , help="Points to scan the Nll at.", metavar="points.txt", default=None)
-    parser.add_argument( "--snapshot"      , type=str,     dest="snapshot"                   , help="Initial snapshot.", default="nominalNuis" )
-    parser.add_argument( "--make-snapshots", action="store_true",    dest="makeParameterSnapshots"     , help="Make parameter snapshots." )
-    parser.add_argument('--fit',                           dest='fit', action='store_true'   , help="Actually run the fit.", default=True )
-    parser.add_argument('--no-fit',                        dest='fit', action='store_false'  , help="Do not run the fit.", default=True )
-    parser.add_argument('--findSigma',                     dest='findSigma', action='store_true' , help="Search for crossings to identify the 1-sigma-band.", default=True )
-    parser.add_argument('--no-findSigma',                  dest='findSigma', action='store_false', help="Do not Search for crossings.", default=True )
-    parser.add_argument('--dummy',                         dest='dummy', action='store_true' , help="Perform a dummy run.", default=False )
+    arglist = []
+    arglist.append(parser.add_argument( "--input"         , type=str,     dest="inFileName"                 , help="File to run over.", required=True, metavar="path/to/workspace.root"))
+    arglist.append(parser.add_argument( "--output"        , type=str,     dest="outFileName"                , help="Output file.", required=False, metavar="out.txt"))
+    arglist.append(parser.add_argument( "--poi"           , type=str,     dest="pois"                       , help="POIs to measure.", metavar="POI", nargs="+", default=[]))
+    arglist.append(parser.add_argument( "--scan"          , nargs=4,      dest="scan"                       , help="POI range to scan the Nll.", metavar=("POI","n","min","max"), default=None))
+    arglist.append(parser.add_argument( "--points"        , type=str,     dest="points"                     , help="Points to scan the Nll at.", metavar="points.txt", default=None))
+    arglist.append(parser.add_argument( "--singlepoint"   , type=str,     dest="point"                      , help="A single point to scan the Nll at.", metavar="POI_A=1,POI_B=0", default=None))
+    arglist.append(parser.add_argument( "--snapshot"      , type=str,     dest="snapshot"                   , help="Initial snapshot.", default="nominalNuis" ))
+    arglist.append(parser.add_argument( "--make-snapshots", action="store_true",    dest="makeParameterSnapshots"     , help="Make parameter snapshots." ))
+    arglist.append(parser.add_argument('--fit',                           dest='fit', action='store_true'   , help="Actually run the fit.", default=True ))
+    arglist.append(parser.add_argument('--no-fit',                        dest='fit', action='store_false'  , help="Do not run the fit.", default=True ))
+    arglist.append(parser.add_argument('--findSigma',                     dest='findSigma', action='store_true' , help="Search for crossings to identify the 1-sigma-band.", default=True ))
+    arglist.append(parser.add_argument('--no-findSigma',                  dest='findSigma', action='store_false', help="Do not Search for crossings.", default=True ))
+    arglist.append(parser.add_argument('--dummy',                         dest='dummy', action='store_true' , help="Perform a dummy run.", default=False ))
     parser.set_defaults(fit=True)
-    parser.add_argument( "--folder"        , type=str,     dest="folder"                     , help="Output folder.", default="test" )
-    parser.add_argument( "--profile"       , type=str,     dest="profile"                    , help="Parameters to profile.", nargs="+", metavar="NP", default=[] )
-    parser.add_argument( "--fix"           , type=str,     dest="fixParameters"              , help="Parameters to fix.", nargs="+", metavar="NP", default=[])
-    parser.add_argument( "--workspace"     , type=str,     dest="wsName"                     , help="WS to grab." , default="combined" )
-    parser.add_argument( "--write-workspace", type=str,    dest="outWsName"                  , help="Filename of the output workspace." , default=None )
-    parser.add_argument( "--modelconfig"   , type=str,     dest="modelConfigName"            , help="MC to load.", default="ModelConfig" )
-    parser.add_argument( "--data"          , type=str,     dest="dataName"                   , help="Data to use.", default="combData" )
-    parser.add_argument( "--minimizerType" , type=str,     dest="minimizerType"              , help="Minimizer type.", default="Minuit2" )
-    parser.add_argument( "--minimizerAlgo" , type=str,     dest="minimizerAlgo"              , help="Minimizer algorithm.", default="Migrad" )
-    parser.add_argument( "--no-hesse"      , action='store_true',   dest="nohesse"	     , help="disable HESSE", default=False )
-    parser.add_argument( "--strategy"      , type=int,     dest="defaultStrategy"            , help="Default strategy.", default=0 )
-    parser.add_argument( "--numCPU"        , type=int,     dest="numCPU"                     , help="Number of CPUs.", default=1 )
-    parser.add_argument( "--numThreads"    , type=int,     dest="numThreads"                 , help="Number of CPU Threads.", default=3 )
-    parser.add_argument( "--binned"        , action='store_true',    dest="binnedLikelihood"           , help="Binned likelihood.", default=True )
-    parser.add_argument( "--unbinned"      , action='store_false',   dest="binnedLikelihood"           , help="Unbinned likelihood.", default=False )
-    parser.add_argument( "--starfix"       , action='store_true',    dest="fixCache"                   , help="Fix StarMomentMorph cache.", default=True )
-    parser.add_argument( "--no-starfix"    , action='store_false',   dest="fixCache"                   , help="Do not fix StarMomentMorph cache.", default=False )
-    parser.add_argument( "--multifix"      , action='store_true',    dest="fixMulti"                   , help="Fix MultiPdf level 2.", default=True )
-    parser.add_argument( "--no-multifix"   , action='store_false',   dest="fixMulti"                   , help="Do not fix MultiPdf level 2.", default=False )
-    parser.add_argument( "--precision"     , type=float,   dest="precision"                  , help="Precision for scan.", default=0.001 )
-    parser.add_argument( "--eps"           , type=float,   dest="eps"                        , help="Convergence criterium.", default=1.0 )
-    parser.add_argument( "--eigen"         , action='store_true',   dest="eigendecomposition"         , help="Eigenvalues and vectors.", default=False )
-    parser.add_argument( "--offset"        , action='store_true',   dest="offsetting"                 , help="Offset likelihood.", default=True )
-    parser.add_argument( "--no-offset"     , action='store_false',  dest="offsetting"                 , help="Do not offset likelihood.", default=False )
-    parser.add_argument( "--reuse-minimizer"        , action='store_true',   dest="reuseMinimizer"                 , help="Allow to reuse the minimizer.", default=False )
-    parser.add_argument( "--no-reuse-minimizer"     , action='store_false',  dest="reuseMinimizer"                 , help="Do not allow to reuse the minimizer.", default=True )
-    parser.add_argument( "--reuse-nll"        , action='store_true',   dest="reuseNll"                 , help="Allow to reuse the nll.", default=False )
-    parser.add_argument( "--no-reuse-nll"     , action='store_false',  dest="reuseNll"                 , help="Do not allow to reuse the nll.", default=True )
-    parser.add_argument( "--initError"     , type=bool,    dest="setInitialError"            , help="Pre-set the initial error.", default=False )
-    parser.add_argument( "--optimize"      , type=int,     dest="constOpt"                   , help="Optimize constant terms." , default=2)
-    parser.add_argument( "--loglevel"      , type=str,     dest="loglevel"                   , help="Verbosity.", choices=["DEBUG","INFO","WARNING","ERROR"], default="DEBUG" )
-    parser.add_argument( "--fixAllNP"      , action='store_true',    dest="fixAllNP"                   , help="Fix all NP.", default=False )
-#   parser.add_argument( "--breakdownErrors", action='store_true',   dest="breakdownErrors"  , help="Breakdown uncertainities into stat., exp. and theo.", default=False )
+    arglist.append(parser.add_argument( "--folder"        , type=str,     dest="folder"                     , help="Output folder.", default="test" ))
+    arglist.append(parser.add_argument( "--profile"       , type=str,     dest="profile"                    , help="Parameters to profile.", nargs="+", metavar="NP", default=[] ))
+    arglist.append(parser.add_argument( "--fix"           , type=str,     dest="fixParameters"              , help="Parameters to fix.", nargs="+", metavar="NP", default=[]))
+    arglist.append(parser.add_argument( "--workspace"     , type=str,     dest="wsName"                     , help="WS to grab." , default="combined" ))
+    arglist.append(parser.add_argument( "--write-workspace", type=str,    dest="outWsName"                  , help="Filename of the output workspace." , default=None ))
+    arglist.append(parser.add_argument( "--modelconfig"   , type=str,     dest="modelConfigName"            , help="MC to load.", default="ModelConfig" ))
+    arglist.append(parser.add_argument( "--data"          , type=str,     dest="dataName"                   , help="Data to use.", default="combData" ))
+    arglist.append(parser.add_argument( "--minimizerType" , type=str,     dest="minimizerType"              , help="Minimizer type.", default="Minuit2" ))
+    arglist.append(parser.add_argument( "--minimizerAlgo" , type=str,     dest="minimizerAlgo"              , help="Minimizer algorithm.", default="Migrad" ))
+    arglist.append(parser.add_argument( "--no-hesse"      , action='store_true',   dest="nohesse"	     , help="disable HESSE", default=False ))
+    arglist.append(parser.add_argument( "--strategy"      , type=int,     dest="defaultStrategy"            , help="Default strategy.", default=0 ))
+    arglist.append(parser.add_argument( "--numCPU"        , type=int,     dest="numCPU"                     , help="Number of CPUs.", default=1 ))
+    arglist.append(parser.add_argument( "--numThreads"    , type=int,     dest="numThreads"                 , help="Number of CPU Threads.", default=3 ))
+    arglist.append(parser.add_argument( "--writeSubmit"   , type=str,     dest="writeSubmit"                , help="Instead of fitting, write a job definition file.", default="jobs.txt" ))
+    arglist.append(parser.add_argument( "--binned"        , action='store_true',    dest="binnedLikelihood"           , help="Binned likelihood.", default=True ))
+    arglist.append(parser.add_argument( "--unbinned"      , action='store_false',   dest="binnedLikelihood"           , help="Unbinned likelihood.", default=False ))
+    arglist.append(parser.add_argument( "--starfix"       , action='store_true',    dest="fixCache"                   , help="Fix StarMomentMorph cache.", default=True ))
+    arglist.append(parser.add_argument( "--no-starfix"    , action='store_false',   dest="fixCache"                   , help="Do not fix StarMomentMorph cache.", default=False ))
+    arglist.append(parser.add_argument( "--multifix"      , action='store_true',    dest="fixMulti"                   , help="Fix MultiPdf level 2.", default=True ))
+    arglist.append(parser.add_argument( "--no-multifix"   , action='store_false',   dest="fixMulti"                   , help="Do not fix MultiPdf level 2.", default=False ))
+    arglist.append(parser.add_argument( "--precision"     , type=float,   dest="precision"                  , help="Precision for scan.", default=0.001 ))
+    arglist.append(parser.add_argument( "--eps"           , type=float,   dest="eps"                        , help="Convergence criterium.", default=1.0 ))
+    arglist.append(parser.add_argument( "--eigen"         , action='store_true',   dest="eigendecomposition"         , help="Eigenvalues and vectors.", default=False ))
+    arglist.append(parser.add_argument( "--offset"        , action='store_true',   dest="offsetting"                 , help="Offset likelihood.", default=True ))
+    arglist.append(parser.add_argument( "--no-offset"     , action='store_false',  dest="offsetting"                 , help="Do not offset likelihood.", default=False ))
+    arglist.append(parser.add_argument( "--reuse-minimizer"        , action='store_true',   dest="reuseMinimizer"                 , help="Allow to reuse the minimizer.", default=False ))
+    arglist.append(parser.add_argument( "--no-reuse-minimizer"     , action='store_false',  dest="reuseMinimizer"                 , help="Do not allow to reuse the minimizer.", default=True ))
+    arglist.append(parser.add_argument( "--reuse-nll"        , action='store_true',   dest="reuseNll"                 , help="Allow to reuse the nll.", default=False ))
+    arglist.append(parser.add_argument( "--no-reuse-nll"     , action='store_false',  dest="reuseNll"                 , help="Do not allow to reuse the nll.", default=True ))
+    arglist.append(parser.add_argument( "--initError"     , type=bool,    dest="setInitialError"            , help="Pre-set the initial error.", default=False ))
+    arglist.append(parser.add_argument( "--optimize"      , type=int,     dest="constOpt"                   , help="Optimize constant terms." , default=2))
+    arglist.append(parser.add_argument( "--loglevel"      , type=str,     dest="loglevel"                   , help="Verbosity.", choices=["DEBUG","INFO","WARNING","ERROR"], default="DEBUG" ))
+    arglist.append(parser.add_argument( "--fixAllNP"      , action='store_true',    dest="fixAllNP"                   , help="Fix all NP.", default=False ))
+
     args = parser.parse_args()
+
+    if args.writeSubmit and args.scan:
+        createScanJobs(args,arglist)
+        exit(0)
+
     setup(args)
     model = buildModel(args)
     minimizer = buildMinimizer(args,model)
