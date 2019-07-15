b064e95fd82b470008a0b6dcb81594cac9b8099d
Rahul Balasubramanian <rahul.balasubramanian@cern.ch>
2019-07-15 23:33:26 +0200

    update functions

--- python/interpolate.py
+++ python/interpolate.py
@@ -3,6 +3,155 @@
 inf = float("inf")
 nan = float("nan")
 
+def findmergecontours(points1, points2, values, smooth):
+    """find the contours in a 2d graph"""
+    from numpy import array
+    from math import isnan
+    keys1 = sorted(points1.keys())
+    keys2 = sorted(points2.keys())
+
+    xvals1 = [ p[0] for p in keys1 ]
+    yvals1 = [ p[1] for p in keys1 ]
+    zvals1 = [ points1[p] for p in keys1 ]
+
+
+    xvals2 = [ p[0] for p in keys2 ]
+    yvals2 = [ p[1] for p in keys2 ]
+    zvals2 = [ points2[p] for p in keys2 ]
+
+    npoints = 50
+
+    from scipy.interpolate import griddata
+    from numpy import mgrid
+    grid1_x, grid1_y = mgrid[min(min(xvals1),min(xvals2)):max(max(xvals2),max(xvals1)):complex(npoints), min(min(yvals1),min(yvals2)):max(max(yvals2),max(yvals1)):complex(npoints)]
+    grid2_x, grid2_y = mgrid[min(min(xvals1),min(xvals2)):max(max(xvals2),max(xvals1)):complex(npoints), min(min(yvals1),min(yvals2)):max(max(yvals2),max(yvals1)):complex(npoints)]
+#    grid1_x, grid1_y = mgrid[min(min(xvals1),min(xvals2)):max(max(xvals2),max(xvals1)):complex(npoints), 0.0:max(max(yvals2),max(yvals1)):complex(npoints)]
+#    grid2_x, grid2_y = mgrid[min(min(xvals1),min(xvals2)):max(max(xvals2),max(xvals1)):complex(npoints), 0.0:max(max(yvals2),max(yvals1)):complex(npoints)]
+
+    print min(min(xvals1),min(xvals2))
+    print max(max(xvals2),max(xvals1))
+    print min(min(yvals1),min(yvals2))
+    print max(max(yvals2),max(yvals1))
+
+    grid1_z = griddata(array(keys1),array(zvals1),(grid1_x, grid1_y), method='cubic')
+    grid2_z = griddata(array(keys2),array(zvals2),(grid2_x, grid2_y), method='cubic')
+
+    #find minima from the grid
+    nllmin1_grid = inf
+    minimum1_grid = (nan,nan)
+    for i in range(0,npoints):
+        for j in range(0,npoints):
+            zval = grid1_z[i][j]
+            if zval < nllmin1_grid:
+                nllmin1_grid = zval
+                minimum1_grid = (grid1_x[i][j],grid1_y[i][j])
+
+    nllmin2_grid = inf
+    minimum2_grid = (nan,nan)
+    for i in range(0,npoints):
+        for j in range(0,npoints):
+            zval = grid2_z[i][j]
+            if zval < nllmin2_grid:
+                nllmin2_grid = zval
+                minimum2_grid = (grid2_x[i][j],grid2_y[i][j])
+
+#perform the evelope on the grid
+    count = 0
+    for i in range(0,npoints):
+        for j in range(0,npoints):
+	    if grid1_x[i][j] == grid2_x[i][j] and grid1_y[i][j] == grid2_y[i][j]:
+                 if grid2_z[i][j] != nan and grid2_z[i][j] != nan:
+                    grid1_z[i][j] = min(grid2_z[i][j],grid1_z[i][j])
+#		    grid1_z[i][j] = grid2_z[i][j]
+#		    grid1_z[i][j] = grid1_z[i][j]
+		    if -0.001 < grid1_x[i][j] < 0.001 and -0.001 <  grid1_y[i][j] < 0.001:
+			print grid1_z[i][j]
+		    count = count + 1
+
+    print count
+    #find nllmin in the enveloped grid
+    minimum = (nan,nan)
+    nllmin = inf
+    for i in range(0,npoints):
+        for j in range(0,npoints):
+            zval = grid1_z[i][j]
+            if zval < nllmin:
+                nllmin = zval
+                minimum = (grid1_x[i][j],grid1_y[i][j])
+
+ #   print nllmin
+#    nllmin = 11112234.9491
+#    nllmin = 11112234.9635
+#    nllmin = 11112236.5508
+
+#    nllmin = 11112236.5508
+#    nllmin = 11112234.9515
+    nllmin = 2156657.18102
+    from skimage import measure
+    allcontours = []
+    allimgcontours = []
+    for v in values:
+        imgcontours = measure.find_contours(grid1_z,v + nllmin)
+        allimgcontours.append(imgcontours)
+        contours = []
+        for c in imgcontours:
+            realc = []
+            for i,j in c:
+                if isnan(i) or isnan(j): continue
+                realc.append((i/npoints * (max(max(xvals1),max(xvals2))-min(min(xvals1),min(xvals2))) + min(min(xvals1),min(xvals2)),j/npoints * (max(max(yvals1),max(yvals2))-min(min(yvals1),min(yvals2))) + min(min(yvals1),min(yvals2))))
+#                realc.append((i/npoints * (max(xvals1)-min(xvals1)) + min(xvals1),j/npoints * (max(yvals1)-min(yvals1)) + min(yvals1)))
+            if len(realc) > 0:
+                if smooth:
+                    contours.append(smoothgraph(realc))
+                else:
+                    contours.append(realc)                    
+                    
+        allcontours.append(contours)
+
+
+    # Display the image and plot all contours found
+#    import matplotlib.pyplot as plt
+#    fig, ax = plt.subplots()
+#    ax.imshow(grid2_z, interpolation='nearest', cmap=plt.cm.coolwarm)
+#    ax.set_zlim(11112230, 11112245)
+#    for contours in allimgcontours:
+#        for n, contour in enumerate(contours):
+#            ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+       
+#    ax.axis('image')
+#    ax.set_xticks([])
+#    ax.set_yticks#([])
+#    plt.show() 
+
+#    from mpl_toolkits.mplot3d import Axes3D
+#    import matplotlib.pyplot as plt
+#    from matplotlib import cm
+#    from matplotlib.ticker import LinearLocator, FormatStrFormatter
+#    import numpy as np
+
+#    fig = plt.figure()
+#    ax = fig.gca(projection='3d')
+
+    # Plot the surface.
+#    surf = ax.plot_surface(grid1_x, grid1_y, grid1_z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+#    surf1 = ax.plot_surface(grid2_x, grid2_y, grid2_z, cmap=cm.Spectral, linewidth=0, antialiased=False)
+
+
+    # Customize the z axis.
+#    ax.set_zlim(11112230, 11112245)
+#    ax.zaxis.set_major_locator(LinearLocator(10))
+#    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+#    surf1.set_clim(11112230,11112245)
+#  
+#    surf.set_clim(11112230,11112245)
+# Add a color bar which maps values to colors.
+#    fig.colorbar(surf, shrink=0.5, aspect=5)
+#    plt.show()
+
+
+    return allcontours,minimum
+ 
+
 def findcontours(points,values,smooth):
     """find the contours in a 2d graph"""
     from numpy import array
@@ -12,34 +161,81 @@ def findcontours(points,values,smooth):
     xvals = [ p[0] for p in keys ]
     yvals = [ p[1] for p in keys ]
     zvals = [ points[p] for p in keys ]
-
-    npoints = 100
-    
+    xyvals = [ [p[0],p[1]] for p in keys ]
+    npoints = 50
     from scipy.interpolate import griddata
     from numpy import mgrid
     grid_x, grid_y = mgrid[min(xvals):max(xvals):complex(npoints), min(yvals):max(yvals):complex(npoints)]
-
+#    grid_x, grid_y = mgrid[-0.20:0.5:complex(npoints), 0.0:0.5:complex(npoints)]
     grid_z = griddata(array(keys),array(zvals),(grid_x, grid_y), method='linear')
 
-    nllmin = inf
-    minimum = (nan,nan)
-    for i in range(0,npoints):
-        if zvals[i] < nllmin:
-            nllmin = zvals[i]
-            minimum = (xvals[i],yvals[i])
+#    nllmin = 11112234.4895
+#    nllmin = 2156649.49679 #hww
+#    nllmin = 11112236.5508 #GenP
+#   nllmin = 11112234.9515
+#    nllmin = 2156657.16678 
+#    nllmin = 2156657.18102
+#    nllmin = 2156649.49679 #hgamgam
+#    nllmin = 2156649.49679 #htautau
+#    nllmin = 2156657.18102 #5xs
+#    nllmin = 11112234.526  #CgCy
+#    nllmin = 2156657.18102 #5xs old
+    nllmin = 2156657.16678 #5xs new 
+#    minimum = (nan,nan)
+#    for i in range(0,npoints):
+#        if zvals[i] < nllmin:
+#            nllmin = zvals[i]
+#            minimum = (xvals[i],yvals[i])
 
+    nllmin_grid = inf
+    minimum_grid = inf
     for i in range(0,npoints):
         for j in range(0,npoints):
             zval = grid_z[i][j]
-            if zval < nllmin:
-                nllmin = zval
-                minimum = (grid_x[i][j],grid_y[i][j])
-                
+            if zval < nllmin_grid:
+                nllmin_grid = zval
+                minimum_grid = (grid_x[i][j],grid_y[i][j])
+
+#    for i in range(0,npoints):
+#	for j in range(0,npoints):
+#	    grid_z[i][j] = grid_z[i][j] - nllmin_grid + nllmin
+#    x_sm = inf
+#    y_sm = inf
+#    z_sm = inf
+#    for i in range(0,npoints):
+#	for j in range(0,npoints):
+#	    if abs(grid_x[i][j]) < 1e-02 and abs(grid_y[i][j]) < 1e-02:
+#		if abs(grid_x[i][j]) < x_sm and  abs(grid_y[i][j]) < y_sm: 
+#		    x_sm = grid_x[i][j]
+#		    y_sm = grid_y[i][j]
+#		    z_sm = grid_z[i][j]
+
+#    print x_sm
+#    print y_sm
+#    print z_sm
+#    for i in range(0,npoints):
+#        for j in range(0,npoints):
+#            if grid_x[i][j] == 0.0 and grid_y[i][j] == 0.0:
+#		print "hello"
+#		print grid_z[i][j]
+
+    print nllmin
+#    print minimum 
+#    nllmin = 11112234.9491
+#    nllmin = 11112234.9635
+
+
+#    nllmin = 11112234.949976524
+#    nllmin = 11112232.932974193
+
+
+#    nllmin = 11112236.5508
+#    nllmin = 11112236.629944898
     from skimage import measure
     allcontours = []
     allimgcontours = []
     for v in values:
-        imgcontours = measure.find_contours(grid_z, v+nllmin)
+        imgcontours = measure.find_contours(grid_z, v + nllmin)
         allimgcontours.append(imgcontours)
         contours = []
         for c in imgcontours:
@@ -62,13 +258,13 @@ def findcontours(points,values,smooth):
 #    for contours in allimgcontours:
 #        for n, contour in enumerate(contours):
 #            ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-#        
+       
 #    ax.axis('image')
 #    ax.set_xticks([])
 #    ax.set_yticks([])
 #    plt.show() 
 
-    return allcontours,minimum
+    return allcontours,minimum_grid
     
 def findcrossings(points,nllval):
     """find the minimum point of a 1d graph and the crossing points with a horizontal line at a given value"""
@@ -126,3 +322,4 @@ def smoothgraph(graph):
             lastx,lasty = x,y
     newgraph.append(newgraph[0])
     return newgraph       
+
--- python/pgfplotter.py
+++ python/pgfplotter.py
@@ -2,17 +2,188 @@
 
 def writehead(stream):
     stream.write("\\documentclass{standalone}\n")
+    stream.write("\\usepackage{scalerel}\n")
     stream.write("\\usepackage{pgfplots,tikz}\n")
     stream.write("\\usetikzlibrary{calc}\n")
+    stream.write("\\usepackage[scaled=1]{helvet}\n")
+    stream.write("\\usepackage[helvet]{sfmath}\n")
+    stream.write("\\usepackage{amsmath,latexsym}\n")
     stream.write("\\usetikzlibrary{shapes.misc}\n")
     stream.write("\\tikzset{cross/.style={cross out, draw=black, minimum size=2*(#1-\pgflinewidth), inner sep=0pt, outer sep=0pt},cross/.default={3pt}}\n")
+    stream.write("\\catcode`_=\\active\n")
+    stream.write("\\newcommand_[1]{\\ensuremath{\\sb{\\scriptscriptstyle #1}}}\n")
+
     stream.write("\\begin{document}\n")
+    stream.write("\\definecolor{myyellow}{rgb}{0.96,0.742,0.29}\n")
+    stream.write("\\definecolor{myblue}{rgb}{0.1,0.32,0.738}\n")
 
 def writefoot(stream):
     stream.write("\\end{document}\n")
 
 def writeATLAS(label,outfile):
     outfile.write("\\node at (rel axis cs:0,1) [anchor=south west,text width=3cm]{\\textbf{ATLAS} "+label+"};\n")
+
+def writepois(atlas,par,allscans,outfilename,ymax=None):
+   """write a pois/NP ranking styled plot to a pgfplots tex file"""
+   with open(outfilename,"w") as outfile:
+	writehead(outfile)
+        allvals = [v[0] for pois in allscans.values() for v in pois.values()]
+	print allvals
+
+def writecorrelations(stream,allcorrs):
+	for x in range(0,len(allcorrs)):
+	    stream.write(allcorrs[x]+"\n")
+
+def concat(strlist):
+    string = ""
+    for x in range(0,len(strlist)):
+      string = string +","+ strlist[x]
+    return string[1:len(string)]
+
+def writecorrmatrix(atlas,parslist,allcorrs,outfilename,ymax=None):
+    """write a correlation matrix to a pgfplots tex file"""
+    paraxisy = parslist
+    paraxisx = parslist[::-1]
+    print parslist
+    with open(outfilename,"w") as outfile:
+	writehead(outfile)
+        outfile.write("\\begin{tikzpicture}\n")
+	outfile.write("\\begin{axis}[\n")
+        outfile.write("    colormap={bluewhiteyellow}{color=(myyellow) color=(white) color=(myblue)},\n")
+        outfile.write("    clip = false,\n")
+        outfile.write("    colorbar,\n")
+        outfile.write("    colormap name={bluewhiteyellow},\n")
+        outfile.write("    x=1em,\n")
+        outfile.write("    y=1em,\n")
+        outfile.write("    xtick=data,\n")
+        outfile.write("    ytick=data,\n")
+        outfile.write("    ymin="+ paraxisy[0] + ",\n")
+        outfile.write("    ymax="+ paraxisy[len(parslist)-1] +",\n")     
+        outfile.write("    xmin="+ paraxisx[0] +",\n")
+        outfile.write("    xmax="+ paraxisx[len(parslist)-1] +",\n")
+        outfile.write("    enlarge x limits={abs=0.5em},\n")
+        outfile.write("    enlarge y limits={abs=0.5em},\n")
+        outfile.write("    point meta min=-1,\n")
+        outfile.write("    point meta max=+1,\n")
+        outfile.write("    grid=both,\n")
+        outfile.write("    major grid style={draw=none},\n")
+	outfile.write("    minor tick num=1,\n")
+        outfile.write("    symbolic x coords={"+ concat(paraxisx) + "},\n")
+        outfile.write("    symbolic y coords={"+ concat(paraxisy) + "},\n")
+	outfile.write("    axis on top,")
+        outfile.write("	   x tick label style={rotate=90},\n")
+        outfile.write("    tick style={draw=none}\n ]\n")
+	outfile.write("\\addplot [matrix plot*,point meta=explicit,mesh/cols="+str(len(parslist))+"] table [meta=correlations] {\n")
+	outfile.write("x  y  correlations\n")
+        writecorrelations(outfile,allcorrs) 
+	outfile.write("};\n")
+	outfile.write("\\node (atlas) [above right, font={\\fontfamily{phv}\\fontseries{b}\selectfont}] at (rel axis cs:0,1) {ATLAS};\n")
+	outfile.write("\\node [anchor=west] at (atlas.east) {Internal};\n")
+        outfile.write("\\end{axis}\n")
+	outfile.write("\\end{tikzpicture}\n")
+        writefoot(outfile)
+
+def writescans1d(atlas,par,allscans,outfilename,ymax=None):
+    """write a bunch of 1d scans to a pgfplots tex file"""
+    with open(outfilename,"w") as outfile:
+        writehead(outfile)
+        allvals = [ v[0] for curves in allscans.values() for scan in curves.values() for v in scan.keys()]
+        domain = "domain={0:f}:{1:f}".format(min(allvals),max(allvals))
+        outfile.write("\\begin{tikzpicture}\n")
+        if atlas: writeATLAS(atlas,outfile)
+        outfile.write("\\begin{axis}[\n")
+        outfile.write("    ymin=0,\n")
+        if ymax:
+            outfile.write("    ymax="+str(ymax)+",\n")
+        outfile.write("    "+domain+",\n")
+        outfile.write("    legend pos=north east,legend style={draw=none},\n")
+        outfile.write("    xlabel=${0:s}$, ylabel=$\\Delta \\log L$\n".format(par))
+        outfile.write("]\n")
+        for pnamelist,curve in allscans.items():
+            for options,scan in curve.items():
+                print("writing scan for "+pnamelist[0])
+                writescan1d(pnamelist[0],par,{k[0]:v for k,v in scan.items()},outfile,ymax)
+        outfile.write("\\end{axis}\n")
+        outfile.write("\\end{tikzpicture}\n")
+        writefoot(outfile)
+
+def writepois(atlas,allpois,outfilename,ymax=None):
+    """write a POI plot to a pgfplots tex file"""
+    from RooFitUtils.io import texprep
+    with open(outfilename,"w") as outfile:
+	writehead(outfile)
+        outfile.write("\\begin{tikzpicture}\n")
+	outfile.write("\\begin{axis}[\n")
+	outfile.write("    width = 0.8\\textwidth,\n")
+	outfile.write("    height = 1\\textwidth, \n")
+	outfile.write("    xlabel = Best fit value \n")
+        outfile.write("    clip = false,\n")
+        outfile.write("    ymin=-1,\n")
+        outfile.write("    ymax= "+str(4.75 + len(allpois)*1.25)+ ",\n")     
+        outfile.write("    xmin= -1.75,\n")
+        outfile.write("    xmax=  2,\n")
+	outfile.write("    minor tick num=4,\n")
+	outfile.write("    ytick style={draw=none},\n")
+        outfile.write("    yticklabels=\empty\n")
+	outfile.write("]")
+	outfile.write("\\addplot+ [color=black, sharp plot,only marks,error bars/.cd,x dir=both, x explicit]\n coordinates{\n")
+	count = 0
+	for x in allpois:
+	   outfile.write("("+str(allpois[x][0])+","+ str(0.25+1.25*count)+") += ("+str(allpois[x][1]) +",0) -= ("+str(allpois[x][2])+",0) \n")
+	   count = count + 1
+	outfile.write("};\n")
+	count = 0
+	for x in allpois:
+	   outfile.write("\\node at ( axis cs:-1.75,"+ str(1.00+1.25*count)+") [anchor = north west]{"+texprep(x)+"};\n")
+	   count = count + 1
+	outfile.write("\\node (atlas) [above right, font={\\fontfamily{phv}\\fontseries{b}\selectfont}] at (rel axis cs:0,1) {ATLAS};\n")
+	outfile.write("\\node [anchor=west] at (atlas.east) {Internal};\n")
+        outfile.write("\\end{axis}\n")
+	outfile.write("\\end{tikzpicture}\n")
+        writefoot(outfile)
+
+def writecorrmatrix(atlas,parslist,allcorrs,outfilename,ymax=None):
+    """write a correlation matrix to a pgfplots tex file"""
+    paraxisy = parslist
+    paraxisx = parslist[::-1]
+    print parslist
+    with open(outfilename,"w") as outfile:
+	writehead(outfile)
+        outfile.write("\\begin{tikzpicture}\n")
+	outfile.write("\\begin{axis}[\n")
+        outfile.write("    colormap={bluewhiteyellow}{color=(myyellow) color=(white) color=(myblue)},\n")
+        outfile.write("    clip = false,\n")
+        outfile.write("    colorbar,\n")
+        outfile.write("    colormap name={bluewhiteyellow},\n")
+        outfile.write("    x=1em,\n")
+        outfile.write("    y=1em,\n")
+        outfile.write("    xtick=data,\n")
+        outfile.write("    ytick=data,\n")
+        outfile.write("    ymin="+ paraxisy[0] + ",\n")
+        outfile.write("    ymax="+ paraxisy[len(parslist)-1] +",\n")     
+        outfile.write("    xmin="+ paraxisx[0] +",\n")
+        outfile.write("    xmax="+ paraxisx[len(parslist)-1] +",\n")
+        outfile.write("    enlarge x limits={abs=0.5em},\n")
+        outfile.write("    enlarge y limits={abs=0.5em},\n")
+        outfile.write("    point meta min=-1,\n")
+        outfile.write("    point meta max=+1,\n")
+        outfile.write("    grid=both,\n")
+        outfile.write("    major grid style={draw=none},\n")
+	outfile.write("    minor tick num=1,\n")
+        outfile.write("    symbolic x coords={"+ concat(paraxisx) + "},\n")
+        outfile.write("    symbolic y coords={"+ concat(paraxisy) + "},\n")
+	outfile.write("    axis on top,")
+        outfile.write("	   x tick label style={rotate=90},\n")
+        outfile.write("    tick style={draw=none}\n ]\n")
+	outfile.write("\\addplot [matrix plot*,point meta=explicit,mesh/cols="+str(len(parslist))+"] table [meta=correlations] {\n")
+	outfile.write("x  y  correlations\n")
+        writecorrelations(outfile,allcorrs) 
+	outfile.write("};\n")
+	outfile.write("\\node (atlas) [above right, font={\\fontfamily{phv}\\fontseries{b}\selectfont}] at (rel axis cs:0,1) {ATLAS};\n")
+	outfile.write("\\node [anchor=west] at (atlas.east) {Internal};\n")
+        outfile.write("\\end{axis}\n")
+	outfile.write("\\end{tikzpicture}\n")
+        writefoot(outfile)
 
 def writescans1d(atlas,par,allscans,outfilename,ymax=None):
     """write a bunch of 1d scans to a pgfplots tex file"""
@@ -41,7 +212,7 @@ def writescans1d(atlas,par,allscans,outfilename,ymax=None):
 def writescan1d(parname,parlabel,allpoints,outfile,ymax=None):
     """write a single 1d scan to a pgfplots tex file"""
     from math import isnan
-    from RooFitUtils.interpolate import findminimum
+    from RooFitUtils.interpolate import findminimum,findcrossings
     from RooFitUtils.util import graphoffset
     othermin = min(allpoints.values())
     nllmin = findminimum(allpoints)
@@ -97,7 +268,10 @@ def writescans2d(args,scans2d,extrapoints):
         if args.atlas: writeATLAS(args.atlas,outfile)
     	# 1 sigma (=68.26895% CL):  2.296
     	# 2 sigma (=95.44997% CL):  6.180
-        contours = {0.5*2.296:"solid",0.5*6.180:"dashed"}
+	# 95% CL		 :  2.28
+        # 68% CL 		 :  5.99
+#        contours = {0.5*2.296:"solid",0.5*6.180:"dashed"}
+        contours = {0.5*2.28:"solid",0.5*5.99:"dashed"}
         for pnamelist,scan in scans2d.items():
             for drawopts,points in scan.items():
                 writescan2d(args,points,outfile,contours,parsedict(drawopts))
