05fc6b79c7de028e0db12a25917b7c2df7253608
Rahul Balasubramanian <rahul.balasubramanian@cern.ch>
2019-07-25 11:35:01 +0200

    update io script

--- python/io.py
+++ python/io.py
@@ -2,6 +2,17 @@
 
 import os
 
+def texprep(string):
+    import re
+    x = ""
+    wilcoeffpat = re.compile("(^c\w+)")
+    mwilcoeff = wilcoeffpat.search(string)
+    if mwilcoeff:
+	x = string.replace("c","$c_{") + "}$"
+        if "box" in x: x = x.replace("box","\\Box")
+    else : x = string.replace("_","\_")
+    return x
+
 def writeResult(out,result,writehesse):
     if result.min.nll:
         out.write("Minimization: minNll = ")
@@ -13,13 +24,14 @@ def writeResult(out,result,writehesse):
         matrix = result.fit.correlationHist()
         out.write("Correlations {:d}\n".format(matrix.GetNbinsX()))
         for i in range (0,matrix.GetXaxis().GetNbins()):
-            out.write(matrix.GetYaxis().GetBinLabel(i+1)+" ")
+            out.write(matrix.GetXaxis().GetBinLabel(i+1)+" ")
         out.write("\n")
         for i in range (0,matrix.GetNbinsX()):
-            out.write(matrix.GetYaxis().GetBinLabel(i+1)+" ")
             for j in range(0,matrix.GetNbinsY()):
+                out.write(matrix.GetXaxis().GetBinLabel(i+1)+" ")
+                out.write(matrix.GetYaxis().GetBinLabel(j+1)+" ")
                 out.write("{:.6f} ".format(matrix.GetBinContent(i+1,j+1)))
-            out.write("\n")                   
+                out.write("\n")                   
     for scan in result.scans:
         out.write((" ".join(scan.parNames)) + " nll status\n")
         for i in range(0,len(scan.nllValues)):
@@ -42,11 +54,53 @@ def collectpoints(points,files,label):
                     point = parsedict(line,float)
                     allpoints.append(point)
     points[label] = allpoints
-                    
+
+def reduceparams(allpars,parfilter):
+    import re
+    skimpars = ""
+    parpat = re.compile("^"+parfilter)
+    for x in allpars:
+	if x.startswith(parfilter) : 
+	  skimpars += " "+ texprep(x)
+    return skimpars[1:len(skimpars)]
+
+def collectcorrelationmatrix(parnames,results,filename,parfilter,label):
+    import re
+    ncorrpat = re.compile("Correlations[ ](\w+)")
+    import glob
+    ncorr = 0
+    if os.path.isfile(filename):
+	with open(filename,"r") as infile:
+	     lines = [line for line in infile ]
+	     for lineno in range(0,len(lines)):
+		     line = lines[lineno]
+	             ncorrmatch = ncorrpat.match(line)
+	             if ncorrmatch:
+		         ncorr = int(ncorrmatch.group(1))
+			 lineno = lineno + 1
+		         parline = lines[lineno]
+		         allpars = parline.split(" ")
+			 allpars = allpars[0:len(allpars)-1]
+		         if len(parfilter) != 0: 
+			   redpars = reduceparams(allpars,parfilter) 
+			   parnames.append(redpars)
+			 else:
+			   parnames.append(texprep(parline))
+			 pattern = ""
+			 for x in range(0, ncorr): 
+			   pattern += "([-]*\d+.\d+)[ ]"
+			 rowpat = re.compile("([a-zA-Z0-9_.]+)[ ]([a-zA-Z0-9_.]+)[ ]"+"([-]*\d+.\d+)") 
+        		 for lineno in range(lineno,len(lines)):
+		           rowmatch = rowpat.match(lines[lineno])
+			   if rowmatch:
+			       par1 = (rowmatch.group(1),rowmatch.group(2))
+			       if (len(parfilter) != 0 and par1[0].startswith(parfilter) and par1[1].startswith(parfilter)) or len(parfilter) == 0:
+			          results.append( texprep((par1[0])) +" "+ texprep((par1[1]))+" "+rowmatch.group(3))
+           
 def collectresults(scans,results,files,label):
     """collect a set of results files and return the contents as a dictionary"""
     import re
-    parpat = re.compile("([a-zA-Z0-9_.a-]+)[ ]*=[ ]*([0-9.naife-]+)[ ]*-[ ]*([0-9.naife-]+)[ ]*\+[ ]*([0-9.naife-]+)[ ]*")
+    parpat = re.compile("([a-zA-Z0-9_.]+)[ ]*=[ ]*([0-9.naife-]+)[ ]*-[ ]*([0-9.naife-]+)[ ]*\+[ ]*([0-9.naife-]+)[ ]*")
     nllpat = re.compile("Minimization:[ ]*minNll[ ]*=[ ]*([0-9.naife-]+)")
     import glob
     filenames = []
@@ -59,7 +113,6 @@ def collectresults(scans,results,files,label):
         if os.path.isfile(filename):
             with open(filename,'r') as infile:
                 lines = [ line for line in infile ]
-#        print lines
                 for lineno in range(0,len(lines)):
                     line = lines[lineno]
                     try:
@@ -85,9 +138,7 @@ def collectresults(scans,results,files,label):
                         else:
                             pvals   = tuple([float(parts[i]) for i in range(0,len(parts)-2)])
                             nllval = float(parts[-2])
-                            status = int(parts[-1])
-                            if status == 0: 
-                                scans[key][label][pvals] = nllval
+                            scans[key][label][pvals] = nllval
                     except KeyError:
                         if nllmatch:
                             print("unable to parse line '"+line.strip()+"' in file '"+filename+"', attempted to parse as nll value")
