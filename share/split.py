735454efec9d8ccd8217a9a2cd890d57f683bf82
Carsten Burgard <cburgard@cern.ch>
2019-12-18 18:05:02 +0100

    added script to split parameters

---
+++ share/split.py
@@ -0,0 +1,65 @@
+
+def main(args):
+    import ROOT
+    infile = ROOT.TFile.Open(args.input)
+    ws = infile.Get(args.name)
+    mc = ws.obj(args.mc)
+    pdf = mc.GetPdf()
+    pdfname = pdf.GetName()
+    pdf.SetName(pdfname+"_old")
+    
+    oldpoi = ws.var(args.old)
+    splits = {}
+    for k in args.splits:
+        elems = k.split(":")
+        splits[elems[0]]=elems[1]
+    for k,v in splits.items():
+        ws.factory("{:s}[{:g},{:g},{:g}]".format(v,oldpoi.getVal(),oldpoi.getMin(),oldpoi.getMax()))
+
+    toreplace = []
+    for f in ws.allFunctions():
+        if not f.dependsOn(oldpoi): continue
+        for g in toreplace:
+            if f.dependsOn(g):
+                continue
+            if g.dependsOn(f):
+                toreplace.remove(g)
+        toreplace.append(f)
+        
+    pdfreplace = {}
+    for f in toreplace:
+        fname = f.GetName()
+        f.SetName(fname+"_old")
+        pdfreplace[f.GetName()]=fname
+        newpar = None
+        for k,v in splits.items():
+            if k in fname:
+                newpar = v
+        if not newpar:
+            raise RuntimeError("function {:s} does not fit any of the categories!".format(fname))
+        s = "EDIT::{:s}({:s},{:s}={:s})".format(fname,f.GetName(),oldpoi.GetName(),newpar)
+        print("replacing {:s} by {:s} in {:s}".format(oldpoi.GetName(),newpar,fname))
+        ws.factory(s)
+
+    ws.factory("EDIT::{:s}({:s},".format(pdfname,pdf.GetName())+",".join([k+"="+v for k,v in pdfreplace.items()])+")")
+    newpdf = ws.pdf(pdfname)
+    mc.SetPdf(newpdf)
+    if newpdf.dependsOn(oldpoi):
+        print("new Pdf still depends on old parameter - not all occurences were replaced!")
+    else:
+        print("all occurences were replaced!")        
+    
+    ws.writeToFile(args.output)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser("NIKHEF Statistics Suite workspace combination script",formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("input", help="Input workspace to be used")
+    parser.add_argument("--output", default="workspace-output.root", help="Output workspace")
+    parser.add_argument("--name", default="combined", help="Workspace name")
+    parser.add_argument("--mc", default="ModelConfig", help="ModelConfig name")    
+    parser.add_argument("--old", required=True, help="old parameter name")
+    parser.add_argument("--splits", nargs="+", help="list of splits to be made", metavar="key:newparameter")
+    
+    args = parser.parse_args()
+    main(args)
