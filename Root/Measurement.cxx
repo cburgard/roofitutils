3bcdeb7ba25cffb3fedba0f26469a380bc020253
Ralf Gugel <rgugel@cern.ch>
2020-01-14 18:17:09 +0100

    hacky attempt at preventing the segfault, to be validated. mainly working around nullpointers by making sure somthing semi-reasonable is provided if no original simPdf is present

--- Root/Measurement.cxx
+++ Root/Measurement.cxx
@@ -180,6 +180,13 @@ void RooFitUtils::Measurement::initialise() {
       }
     }
     delete pdfItr;
+    //FIXME: handle if no RooSimultaneous is found...
+    if (!thisSim) { //we still don't have a RooSimultaneous, so let's create a minimal one
+      RooCategory* channelCat = (RooCategory*) fWorkSpace->factory(TString::Format("channelCat[%s]",fPdf->GetName()).Data());
+      std::map< std::string, RooAbsPdf *> pdfMap { std::make_pair(fPdf->GetName(), fPdf) };
+      thisSim = new RooSimultaneous("simPdf","",pdfMap,*channelCat);
+      fWorkSpace->import(*thisSim, RooFit::RecycleConflictNodes());
+    }
   } else {
     coutF(InputArguments) << "Measurement::initialise(" << fName
                           << ") Did not find a RooSimultaneous." << std::endl;
@@ -197,9 +204,16 @@ void RooFitUtils::Measurement::initialise() {
                               fBinnedCategories, fUnbinnedCategories,
                               fWeightVarName);
   }
-
+  
   RooCategory *cat = (RooCategory *)&thisSim->indexCat();
   dataList = fData->split(*cat, true);
+  //FIXME: fData may be from a non-simPdf but a plain ProdPdf -> emulate creation of the list
+  if (!dataList) {
+    dataList = new TList();
+    fData->SetName(fPdf->GetName()); //hack(?)
+    dataList->Add(fData);
+  }
+  std::cout<<"datalist is "<<dataList<<std::endl;
   fObservables = (RooArgSet *)fModelConfig->GetObservables();
   fGlobalObservables = (RooArgSet *)fModelConfig->GetGlobalObservables();
   fNuisanceParameters = (RooArgSet *)fModelConfig->GetNuisanceParameters();
@@ -327,8 +341,9 @@ void RooFitUtils::Measurement::CollectChannels() {
     }
 
     // Get the pdf
+    std::cout<<"trying to get Pdf "<<label.c_str()<<std::endl;
     RooAbsPdf *thisPdf = (RooAbsPdf *)thisSim->getPdf(label.c_str());
-
+    std::cout<<" ptr: "<<thisPdf<<std::endl;
     // Attach all (necessary) constraints to the current channel to make it
     // stand-alone
     RooArgSet thistmpNuisanceParameters;
