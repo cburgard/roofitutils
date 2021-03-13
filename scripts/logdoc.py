6a531f5e21c75a27ac9810a3a65f44c4059adf25
Carsten Burgard <cburgard@cern.ch>
2021-03-13 23:48:24 +0100

    prettyprinting

--- scripts/logdoc.py
+++ scripts/logdoc.py
@@ -1,6 +1,6 @@
 #!/bin/env python
     
-def parselines(elements,lines):
+def parse(messages,lines):
     from RooFitUtils.logfile_diagnostics import make_parser
     parser = make_parser()
     
@@ -13,24 +13,36 @@ def parselines(elements,lines):
     from more_itertools import peekable
     logfile = peekable(lines)
     
-    parser.parse(elements,logfile)
+    parser.parse(messages,logfile)
 
+def printdict(d,indent=0):
+    for k,v in d.items():
+        if isinstance(v,dict):
+            print(" "*4*indent + k)
+            printdict(v,indent+1)            
+        if isinstance(v,list):
+            for e in v:
+                print(" "*4*indent + k)            
+                printdict(e,indent+1)
+        else:
+            print(" "*4*indent + k + " " + v)
             
 def main(args):
-    elements = {}
+    messages = {}
     with open(args.logfile,"rt") as logfile:
-        parselines(elements,logfile)
-    for category in elements.keys():
-        print("{:s}: {:d}".format(category,len(elements[category])))
+        parse(messages,logfile)
+    for category in messages.keys():
+        print("{:s}: {:d}".format(category,len(messages[category])))
 
     for category in args.printcategories:
         print("the following messages were encountered in category "+category+":")
-        for element in elements[category]:
-            print(element)
+        for message in messages[category]:
+            print(category)
+            printdict(message,1)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
-    parser = ArgumentParser("log file doctor")
+    parser = ArgumentParser(description="the log file doctor")
     parser.add_argument("logfile")
     parser.add_argument("--print",dest="printcategories",nargs="+",type=str,default=[])
     parser.add_argument("--debug",dest="debugcategory",type=str,default=None)        
