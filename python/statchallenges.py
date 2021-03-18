c4e5ab8f96ded322c9da0aefd1607a501ce94fcb
Carsten Burgard <cburgard@cern.ch>
2021-03-18 14:59:44 +0100

    added instructions

--- python/statchallenges.py
+++ python/statchallenges.py
@@ -1,3 +1,21 @@
+# this is the implementation of the ATLAS statistics challenges
+#
+# First, check out and compile RooFitUtils
+#
+#     git clone ssh://git@gitlab.cern.ch:7999/cburgard/RooFitUtils.git
+#     mkdir build
+#     cd build
+#     cmake ..
+#     make -j4
+#     source setup.sh
+#     cde ..
+#
+# Then, run the challenges
+#
+#     git clone ssh://git@gitlab.cern.ch:7999/cburgard/statchallenges.git
+#     cd statchallenges
+#     pytest --subjects=../RooFitUtils/python/statchallenges.py -k simplehf
+
 def runfit(filename,workspacename,moreargs={}):
     from RooFitUtils.fit import setup,buildModel,buildMinimizer,fit
     from RooFitUtils.io import result2dict
@@ -9,14 +27,16 @@ def runfit(filename,workspacename,moreargs={}):
     result = fit(args,model,minimizer)
     return result2dict(result)
 
-def test_poivals(filename,workspacename):
+def test_simplehf_poivals(filename,workspacename):
     return runfit(filename,workspacename)["min"]["parameters"]
 
-def test_poiscan(filename,workspacename,scanpars):
+def test_simplehf_poiscan(filename,workspacename,scanpars):
     result = runfit(filename,workspacename,{"scan":scanpars})["scans"]["mu"]
     return [ point["nll"] for point in result ]
 
-def test_covmat(filename,workspacename):
+def test_simplehf_covmat(filename,workspacename):
     from RooFitUtils.io import dict2mat
     return dict2mat(runfit(filename,workspacename,{"correlationMatrix":True})["min"]["cov"])
 
+def test_eft_poivals(filename,workspacename):
+    return runfit(filename,workspacename)["min"]["parameters"]
