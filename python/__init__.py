1717d4cc0de50bc098a35d8ca6aa2fc2a982a9c5
Carsten Burgard <cburgard@cern.ch>
2021-04-10 19:58:32 +0200

    improved error reporting

--- python/__init__.py
+++ python/__init__.py
@@ -4,7 +4,7 @@ try:
     ROOT.RooFitUtils.ExtendedModel
     for k in dir(ROOT.RooFitUtils):
         locals()[k] = getattr(ROOT.RooFitUtils,k)
-except AttributeError or ImportError:
-    raise ImportError("unable to import RooFitUtils")
+except AttributeError or ImportError as err:
+    raise ImportError("unable to import RooFitUtils: "+err)
 
 
