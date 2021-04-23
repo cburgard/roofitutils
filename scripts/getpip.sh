b310b2a847607dd163e636c4e0050286c1c915ec
Carsten Burgard <cburgard@cern.ch>
2021-04-23 19:02:03 +0200

    working on pip acquisition

--- scripts/getpip.sh
+++ scripts/getpip.sh
@@ -1,6 +1,10 @@
-PYTHONVER=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
-rm -rf get-pip.py
-wget -nv https://bootstrap.pypa.io/pip/$PYTHONVER/get-pip.py
-python get-pip.py --user
+if ! command -v pip &> /dev/null; then
+    PYTHONVER=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
+    rm -rf get-pip.py
+    wget -nv https://bootstrap.pypa.io/pip/$PYTHONVER/get-pip.py
+    python get-pip.py --user   
+fi
+
 python -m pip install --user scipy
 python -m pip install --user scikit-image
+   
