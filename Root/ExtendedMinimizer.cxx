0a2a760139692bd1170ec26ffb54d8fc4301f713
Carsten Burgard <cburgard@cern.ch>
2021-11-12 14:25:10 +0100

    refacotring function handling

--- Root/ExtendedMinimizer.cxx
+++ Root/ExtendedMinimizer.cxx
@@ -10,6 +10,7 @@
 #include "TMath.h"
 #include "TRandom.h"
 #include "TMatrixDSymEigen.h"
+#include "RooMinimizerFcn.h"
 
 #include "RooCmdConfig.h"
 #include "RooFitResult.h"
@@ -153,7 +154,7 @@ namespace {
 namespace {
   class RooMinimizerHack : public RooMinimizer{
   public:
-    RooMinimizerFcn* getFitterFcn(){ return this->fitterFcn(); };
+    RooAbsMinimizerFcn* getFitterFcn(){ return this->fitterFcn(); };
   };
 }
 
@@ -199,7 +200,7 @@ namespace {
   int countFloatParams(RooMinimizer* minimizer){
     return (minimizer->*RooMinimizerHackResult<RooMinimizergetNPar>::ptr)();
   }
-  RooMinimizerFcn* fitterFcn(RooMinimizer* minimizer){
+  RooAbsMinimizerFcn* fitterFcn(RooMinimizer* minimizer){
     return ((::RooMinimizerHack*) minimizer)->getFitterFcn();
   }
   bool init(ROOT::Fit::Fitter* fitter){
@@ -263,7 +264,7 @@ int RooFitUtils::ExtendedMinimizer::runHesse(RooFitUtils::ExtendedMinimizer::Res
     std::cout << "ExtendedMinimizer::runHesse(" << fName << ") running standalone (this might take a while) ... "<<std::endl;        
     auto* fcn = fitterFcn(fMinimizer);
     fcn->Synchronize(fitter->Config().ParamsSettings(),fOptConst,0);
-    fitter->SetFCN(*fcn);
+    fitter->SetFCN(*dynamic_cast<RooMinimizerFcn*>(fcn));
     fitter->EvalFCN();          
     init(fitter);
     minimizer = fitter->GetMinimizer();
