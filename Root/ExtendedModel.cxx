2cfb1b0e310dd37d1f517e8b81b5904cc14ff774
Carsten Burgard <cburgard@cern.ch>
2021-11-18 12:57:24 +0100

    root compat

--- Root/ExtendedModel.cxx
+++ Root/ExtendedModel.cxx
@@ -7,7 +7,12 @@
 #include "RooGaussian.h"
 #include "RooRealSumPdf.h"
 #include "RooRealVar.h"
+
+#if ROOT_VERSION_CODE < ROOT_VERSION(6,25,0)
 #include "RooNameSet.h"
+#else
+#include "RooFitLegacy/RooNameSet.h"
+#endif
 
 #include "RooFitUtils/ExtendedModel.h"
 #include "RooFitUtils/Log.h"
