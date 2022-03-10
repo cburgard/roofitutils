a38cca14a713120133a5c7bb487323a9c7471ea6
Carsten Burgard <carsten.burgard@gmail.com>
2022-03-10 17:39:25 +0100

    used executability of scripts

--- test/test_logdoc.sh
+++ test/test_logdoc.sh
@@ -1,2 +1,2 @@
-scripts/fit.py --input test/test_workspace.root --workspace Test --data obsData --hesse --logsave log.txt
-scripts/logdoc.py log.txt --fail-on-print --print unknown
+fit.py --input test/test_workspace.root --workspace Test --data obsData --hesse --logsave log.txt
+logdoc.py log.txt --fail-on-print --print unknown
--- test/test_simplescan.sh
+++ test/test_simplescan.sh
@@ -1,3 +1,3 @@
-scripts/fit.py --input test/test_workspace.root --workspace Test --data asimovData --hesse --scan mu 10 0 2 --print --output scan1d.json
-scripts/plotscan.py --input color=red scan1d.json --output scan1d.tex
+fit.py --input test/test_workspace.root --workspace Test --data asimovData --hesse --scan mu 10 0 2 --print --output scan1d.json
+plotscan.py --input color=red scan1d.json --output scan1d.tex --label "\mu"
 # pdflatex scan1d.tex
