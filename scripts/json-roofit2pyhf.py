45e08ef5ac85df4d3e4b5ce96057c1c6979e445e
Carsten Burgard <carsten.burgard@gmail.com>
2022-06-08 16:23:59 +0200

    some improvements

--- scripts/json-roofit2pyhf.py
+++ scripts/json-roofit2pyhf.py
@@ -11,30 +11,66 @@ def stripname(name,stripcomponents):
             if name.startswith(c):
                 name = name[len(c):].strip("_")
                 anymatch = True
-                break            
+                break
+            if c in name:
+                name = name.replace("_"+c+"_","_")
+                anymatch = True
+                break
         if not anymatch:
             return name
 
 def main(args):
     import re
     import json
+    
+    # read the input
     with open(args.input,"rt") as input:
         input_json = json.load(input)
 
-    measurement = {"name":"meas","config":{"parameters":[],"poi":args.poi}}
+    # setup the main structure
+    pois = [ name for name,var in input_json["variables"].items() if "poi" in var["tags"] ]
+    measurement = {"name":"meas","config":{"parameters":[],"poi": args.poi if args.poi else pois[0]}}
     output_json = {"channels":[],"measurements":[measurement],"observations":[],"version":"1.0.0"}
 
+    # some bookkeeping
     nps = set()
     nfs = set()    
-    
+    channelnames = []
+
+    # define observations / data
+    data = input_json["data"]["obsData"]
+    for key,channel in data.items():
+        if type(channel) != dict:
+            continue
+        channelname = stripname(key,args.strip_name_components)
+        channelnames.append(channelname)
+        if "counts" in channel.keys():
+            output_json["observations"].append({"data":channel["counts"],"name":channelname})        
+        else:
+            output_json["observations"].append({"data":channel["weights"],"name":channelname})
+
+    # define model /pdf
     simpdf = [ pdf for pdf in input_json["pdfs"].values() if pdf["type"] == "simultaneous" ][0]
     for key,channel in simpdf["channels"].items():
-        out_channel = {"name":stripname(key,args.strip_name_components),"samples":[]}
+        if "name" in channel.keys(): key = channel["name"]
+        channelname = stripname(key,args.strip_name_components)
+        for c in channelnames:
+            if c.beginswith(channelname):
+                channelname = c
+                
+        out_channel = {"name":channelname,"samples":[]}
         output_json["channels"].append(out_channel)
         for key,sample in channel["samples"].items():
+            values = sample["data"]["counts"]
+
             out_sample = {"name":stripname(key,args.strip_name_components),"modifiers":[]}
-            out_channel["samples"].append(out_sample)                    
-            out_sample["data"] = sample["data"]["counts"]
+            out_channel["samples"].append(out_sample)
+            out_sample["data"] = values
+
+            if sample["statError"]:
+                errors = sample["data"]["errors"]
+                out_sample["modifiers"].append({"name":"staterror_"+channelname,"type":"staterror","data":[e/v for e,v in zip(errors,values)]})
+            
             if "normFactors" in sample.keys():
                 for nf in sample["normFactors"]:
                     if any([ re.match(b,nf) for b in args.nf_blacklist]): continue                    
@@ -60,21 +96,22 @@ def main(args):
                     nps.add(key)
                     out_sample["modifiers"].append({"type":"normsys","name":key,"data":{"hi":overallSys["high"],"lo":overallSys["low"]}})
 
+    # define parameters
     for par in nps:
         measurement["config"]["parameters"].append({"bounds":[[-1,1]],"fixed":False,"inits":[0.],"name":par})
     for par in nfs:
         if any([ re.match(b,par) for b in args.nf_blacklist]): continue
-        measurement["config"]["parameters"].append({"bounds":[[0,50]],"fixed":False,"inits":[1.],"name":par})        
-                    
-    data = input_json["data"]["obsData"]
-    for key,channel in data.items():
-        if type(channel) != dict:
-            continue
-        if "counts" in channel.keys():
-            output_json["observations"].append({"data":channel["counts"],"name":stripname(key,args.strip_name_components)})
-        else:
-            output_json["observations"].append({"data":channel["weights"],"name":stripname(key,args.strip_name_components)})            
-                    
+        measurement["config"]["parameters"].append({"bounds":[[0,50]],"fixed":False,"inits":[1.],"name":par})
+
+    # some post-processing
+    if args.fix_other_pois:
+        for p in measurement["config"]["parameters"]:
+            for poi in pois:
+                if poi == args.poi: continue
+                if p["name"] == poi:
+                    p["fixed"] = True            
+
+    # write output
     with open(args.output,"wt") as output:
         json.dump(output_json,output,sort_keys=True)
                 
@@ -82,6 +119,7 @@ if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser(description="format converter between ROOT JSON and pyhf JSON")
     parser.add_argument("--poi",default=None)
+    parser.add_argument("--fix-other-pois",action="store_true")
     parser.add_argument("--nf-blacklist",nargs="+",default=["binWidth_.*"],dest="nf_blacklist")
     parser.add_argument("--strip-name-components",nargs="+",default=[])
     parser.add_argument("--defactorize",action="store_true",help="merge OverallSys and HisotSys of the same name",default=False)
