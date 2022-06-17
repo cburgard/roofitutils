311a41128e9abbe92aaf507f545da98f149ec12a
Carsten Burgard <carsten.burgard@gmail.com>
2022-06-17 20:06:02 +0200

    added option to pick import and export json configs

--- scripts/ws2json.py
+++ scripts/ws2json.py
@@ -11,11 +11,16 @@ def main(args):
     if not ws:
         print("no workspace found in file!")
         exit(1)
-    
-    etcDir = str(ROOT.TROOT.GetEtcDir())
-    from os.path import join as pjoin
-    ROOT.RooJSONFactoryWSTool.loadExportKeys(pjoin(etcDir,"RooFitHS3_wsexportkeys.json"))
-    ROOT.RooJSONFactoryWSTool.loadFactoryExpressions(pjoin(etcDir,"RooFitHS3_wsfactoryexpressions.json"))
+
+    if args.default_defs:
+        etcDir = str(ROOT.TROOT.GetEtcDir())
+        from os.path import join as pjoin
+        ROOT.RooJSONFactoryWSTool.loadExportKeys(pjoin(etcDir,"RooFitHS3_wsexportkeys.json"))
+        ROOT.RooJSONFactoryWSTool.loadFactoryExpressions(pjoin(etcDir,"RooFitHS3_wsfactoryexpressions.json"))
+    for arg in args.export_defs:
+        ROOT.RooJSONFactoryWSTool.loadExportKeys(arg)
+    for arg in args.import_defs:
+        ROOT.RooJSONFactoryWSTool.loadFactoryExpressions(arg)        
     tool = ROOT.RooJSONFactoryWSTool(ws)
     tool.exportJSON(args.outfile)
 
@@ -24,5 +29,9 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="turn a workspace into JSON")
     parser.add_argument("infile",help="the ROOT workspace")
     parser.add_argument("outfile",help="the JSON file")
+    parser.add_argument("--no-default-definitions",action="store_false",dest="default_defs")
+    parser.add_argument("--default-definitions",action="store_true",dest="default_defs",default=True)
+    parser.add_argument("--load-export-definitions",dest="export_defs",nargs="+",default=[])
+    parser.add_argument("--load-import-definitions",dest="import_defs",nargs="+",default=[])    
 
     main(parser.parse_args())    
